282bb847e971d7d300f6070710363004acc46e83
s-mondalsh <s-mondalsh@53S500038239.it.bsd405.org>
2019-11-22 12:34:27 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -9,19 +9,67 @@ import java.util.*;
 public class FracCalc {
 
 	public static String produceAnswer(String input) {
-		Scanner s = new Scanner(input);
-		String firstOperand = s.next();
-		
-		String operator = s.next();
-		String secondOperand = s.next();
+		//Scanner s = new Scanner(input);
+		// String firstOperand = s.next();
 
-		return secondOperand;
+		// String operator = s.next();
+		// String secondOperand = s.next();
+
+		// return secondOperand;
+		String wow = input;
+		String operand1 = wow.substring(0, wow.indexOf(' '));
+		wow = wow.substring(wow.indexOf(' ') + 1);
+		String operator = wow.substring(0, wow.indexOf(' '));
+		wow = wow.substring(wow.indexOf(' ') + 1);
+		String operand2 = wow.substring(0);
+
+		String op2whole = findWhole(operand2);
+		String op2num = findNum(operand2);
+		String op2denom = findDenom(operand2);
+
+		String chk2Answer = "whole:" + op2whole + " numerator:" + op2num + " denominator:" + op2denom;
+
+		return chk2Answer;
+
+	}
+
+	public static String findWhole(String str) {
+		if (str.contains("_")) {
+			return str.substring(0, str.indexOf('_'));
+		} else if (str.contains("/")) {
+			return "0";
+
+		} else
+			return str;
+
+	}
+
+	public static String findNum(String str) {
+		if (str.contains("_")) {
+			return str.substring(str.indexOf('_') + 1, str.indexOf('/'));
+		} else if (str.contains("/")) {
+			return str.substring(0, str.indexOf('/'));
+		} else {
+			return "0";
+		}
+	}
+
+	public static String findDenom(String str) {
+		if (str.contains("/")) {
+			return str.substring(str.indexOf("/") + 1);
+		} else {
+			return "1";
+		}
 	}
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		String userResponce = s.nextLine();
-		
-		System.out.println((produceAnswer(userResponce)));
+		String userResponse = s.nextLine();
+		while (!userResponse.equalsIgnoreCase("quit")) {
+			System.out.println((produceAnswer(userResponce)));
+
+		}
 	}
+
 }
